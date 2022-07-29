021cee538f787d73deb6de93a9e91b7afba99833
Dhriti2010 <89203702+Dhriti2010@users.noreply.github.com>
2022-07-29 16:42:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 function preload(){
-
+clownnose=loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 }
 function setup(){
-    canvas=createCanvas(400,400);
+    canvas=createCanvas(300,300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    posenet=ml5.poseNet(video,modelloaded);
+    posenet.on('pose',gotposes);
 }
 function draw(){
-
+image(video,0,0,300,300);
+image(clownnose,nosex,nosey,80,35);
 }
 function take_snapshot(){
     save("myfilterimage.jpg");
+}
+function modelloaded(){
+    console.log("model is loaded");
+}
+function gotposes(results){
+    if (results.length>0){
+        console.log(results);
+        nosex=results[0].pose.nose.x-40;
+        nosey=results[0].pose.nose.y;
+    }
 }
